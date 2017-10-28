4b7c0cf3a898c10b2776c52cba0ac34690bbbf91
Avasth306 <avashs306@gmail.com>
2017-10-28 18:38:38 +0545

    Add files via upload

---
+++ real test/assets/js/jquery.js
@@ -0,0 +1,11 @@
+(function() {
+  $(document).ready(function() {
+    return $(window).scroll(function() {
+      return $(window).scrollTop() > 150 ? $("#back-to-top").addClass("show") : $("#back-to-top").removeClass("show")
+    }), $("#back-to-top").click(function() {
+      return $("html,body").animate({
+        scrollTop: "0"
+      })
+    })
+  })
+}).call(this);
